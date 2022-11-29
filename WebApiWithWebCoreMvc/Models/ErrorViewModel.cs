ea0c406282121ba561b8ec67af18e6a278c2612e
kavlak <kavlak@DESKTOP-KE2QJP9>
2022-11-30 00:11:10 +0300

    Add project files.

---
+++ WebApiWithWebCoreMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebApiWithWebCoreMvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
